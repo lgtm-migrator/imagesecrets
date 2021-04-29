0212dab9aab492a78ff79e4dae3b4e2c3bae9bae
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-04-29 15:14:01 +0200

    Add test for encode_message function

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -13,24 +13,57 @@ def image_arr():
 
 
 @pytest.mark.parametrize(
-    "text, result",
+    "text",
     [
-        ("", ""),
-        ("123", "001100010011001000110011"),
-        ("abc", "011000010110001001100011"),
-        ("1a*", "001100010110000100101010"),
-        ("1 a", "001100010010000001100001"),
-        (" 1a ", "00100000001100010110000100100000"),
-        (".", "00101110"),
-        (
-            "long_text... ",
-            "01101100011011110110111001100111010111110111010001100101011110000111010000101110001011100010111000100000",
-        ),
+        "",
+        "123",
+        "abc",
+        "1a*",
+        "1 a",
+        " 1a ",
+        ".",
+        "qwertyasdf...",
+        "\\\\\\",
+        "~!@#$%$^&*(*)+",
     ],
 )
-def test_str_into_bin_array(text, result):
+def test_str_into_bin_array(text):
+    result = "".join(
+        map(
+            lambda char: format(char, "b").zfill(8),
+            bytearray(text, encoding="utf-8"),
+        )
+    )
+
     arr = encode.str_into_bin_array(text)
     data = "".join(
         (format(i, "b").zfill(8)[-1] for i, _ in zip(arr.flatten(), range(len(result))))
     )
+
     assert data == result
+
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        "",
+        "123",
+        "abc",
+        "1a*",
+        "1 a",
+        " 1a ",
+        ".",
+        "qwertyasdf... ",
+        "\\\\\\",
+        "~!@#$%$^&*(*)+",
+    ],
+)
+def test_encode_message(image_arr, text):
+    new_arr = encode.encode_message(image_arr, text)
+    extracted_text = ""
+    for rgb_vals, _ in zip(new_arr.reshape(-1, 8), range(len(text))):
+        binary = (bin(num)[-1] for num in rgb_vals)
+        char = chr(int("".join(binary), base=2))
+        extracted_text += char
+
+    assert text == extracted_text
