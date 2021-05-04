c0895e5a0a9488cc8791ff4208b8c16d8cacde4a
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-05-04 13:24:36 +0200

    add conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,21 @@
-"""Test that the encode widget workks properly."""
+"""Session wide fixtures."""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 import pytest
 
 from image_secrets.gui.interface import ImageSecretsWindow
 
 if TYPE_CHECKING:
     from PyQt5.QtWidgets import QMainWindow
-    from pytestqt.qtbot import QtBot
+
+
+@pytest.fixture(scope="session")
+def image_arr():
+    """Return array containing data about a random image."""
+    return np.random.randint(0, 255, size=(256, 3), dtype=np.uint8)
 
 
 @pytest.fixture()
@@ -18,13 +24,3 @@ def application(qtbot) -> QMainWindow:
     test_app = ImageSecretsWindow()
     qtbot.addWidget(test_app)
     return test_app
-
-
-def test_submit_button(qtbot: QtBot, application: QMainWindow) -> None:
-    """Test the encode_image_btn.
-
-    :param qtbot: QtBot instance
-    :param application: The application to test
-
-    """
-    btn = getattr(application.ui, "encode_submit_btn")
