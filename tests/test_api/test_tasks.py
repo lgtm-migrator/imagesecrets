45874551d33b4d108d5e9c0e1f80a4b95ba1fb7c
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-06-04 21:42:47 +0200

    add test for repeat task

--- tests/test_api/test_tasks.py
+++ tests/test_api/test_tasks.py
@@ -1,5 +1,35 @@
 """Test API tasks module."""
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING
+
+import pytest
+
+from image_secrets.api import tasks
+
+if TYPE_CHECKING:
+    from pytest_mock import MockFixture
 
 
-def test_repeat() -> None:
-    """ """
+@pytest.fixture()
+def mock_sleep(mocker: MockFixture):
+    """Return mocked ``asyncio.sleep``."""
+    async_mock = mocker.AsyncMock(return_value=None)
+    return mocker.patch(
+        "asyncio.sleep",
+        side_effect=async_mock,
+    )
+
+
+def test_repeat(mock_sleep) -> None:
+    """Test a successful repeat function call."""
+    loop = asyncio.get_event_loop()
+
+    @tasks.repeat(seconds=1)
+    async def test_coro() -> None:
+        """Testing coroutine."""
+
+    loop.create_task(test_coro())
+
+    ...
