4c099766b662d45effc631899359118f1a83f12a
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-08-26 22:37:52 +0200

    generalize update_version script

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,24 +1,53 @@
 """Simple CLI tool to update version of the application."""
 from __future__ import annotations
 
-import argparse
+from argparse import ArgumentParser
+from enum import Enum
 from pathlib import Path
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from argparse import ArgumentParser
 
 BASE = Path(__file__).parent.parent
-PYPROJECT_PATH = BASE / "pyproject.toml"
 INIT_PATH = BASE / "image_secrets/__init__.py"
+PYPROJECT_PATH = BASE / "pyproject.toml"
+
+INIT_START = "__version__ = "
+PYPROJECT_START = "version = "
 
 
-def update_line_in_file(file: Path, line_start: str, new_value: str) -> None:
+class LineStart(str, Enum):
+    """Enumeration of all `LineStart` values to identify each version line."""
+
+    init = INIT_START
+    pyproject = PYPROJECT_START
+
+
+def get_init_line(version: str) -> str:
+    """Return full version line to write into ``__init__.py`` file.
+
+    :param version: New version string
+
+    """
+    return f'{INIT_START}"{version}"\n'
+
+
+def get_pyproject_line(version: str) -> str:
+    """Return full version line to write into ``pyproject.toml`` file.
+
+    :param version: New version string
+
+    """
+    return f'{PYPROJECT_START}"{version}"\n'
+
+
+def update_line_in_file(
+    file: Path,
+    line_start: LineStart,
+    new_line: str,
+) -> None:
     """Update a specific line to a new value.
 
     :param file: Path to the file to update
     :param line_start: Identifier of the line to update
-    :param new_value: New value to insert into the identified line
+    :param new_line: New line to insert into the identified line
 
     """
     with file.open() as input:
@@ -26,16 +55,13 @@ def update_line_in_file(file: Path, line_start: str, new_value: str) -> None:
 
     with file.open(mode="w") as output:
         for line in lines:
-            if line.startswith(line_start):
-                to_write = f"{line_start}{new_value}\n"
-            else:
-                to_write = line
+            to_write = new_line if line.startswith(line_start.value) else line
             output.write(to_write)
 
 
 def get_parser() -> ArgumentParser:
     """Return Parser for CLI arguments."""
-    p = argparse.ArgumentParser()
+    p = ArgumentParser()
     p.add_argument(
         "version",
         type=str,
@@ -44,10 +70,42 @@ def get_parser() -> ArgumentParser:
     return p
 
 
+def update_init(version: str) -> None:
+    """Update the version ``__init__.py`` file.
+
+    param version: New application version
+
+    """
+    new_line = get_init_line(version=version)
+    update_line_in_file(
+        file=INIT_PATH,
+        line_start=LineStart.init,
+        new_line=new_line,
+    )
+
+
+def update_pyproject(version: str) -> None:
+    """Update the version in ``pyproject.toml`` file.
+
+    param version: New application version
+
+    """
+    new_line = get_pyproject_line(version=version)
+    update_line_in_file(
+        file=PYPROJECT_PATH,
+        line_start=LineStart.pyproject,
+        new_line=new_line,
+    )
+
+
+UPDATE_FUNCTIONS = (update_init, update_pyproject)
+
+
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    version = args.version
-    update_line_in_file(PYPROJECT_PATH, "version = ", f'"{version}"')
-    update_line_in_file(INIT_PATH, "__version__ = ", f'"{version}"')
+    ver = args.version
+
+    for function in UPDATE_FUNCTIONS:
+        function(version=ver)
