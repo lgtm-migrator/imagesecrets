4fccca53ff9013c1df6ed74f8a6a0cabf43a7944
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-08-29 16:46:37 +0200

    update relationships

--- imagesecrets/database/image/models.py
+++ imagesecrets/database/image/models.py
@@ -1,7 +1,10 @@
 """Image database models."""
+from __future__ import annotations
+
 from imagesecrets.constants import MESSAGE_DELIMITER
 from imagesecrets.database.base import Base
 from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy.orm import relationship
 
 
 class Image:
@@ -19,11 +22,15 @@ class DecodedImage(Image, Base):
 
     user_id = Column(Integer, ForeignKey("user.id"))
 
+    parent = relationship("User", back_populates="decodedimage")
+
 
 class EncodedImage(Image, Base):
-    """Tortoise encoded image model."""
+    """Encoded image model."""
 
     user_id = Column(Integer, ForeignKey("user.id"))
+
+    parent = relationship("User", back_populates="encodedimage")
 
 
 __all__ = [
