0c5ff8e03c6f9bd9770d5f8309af8aaa5b21c883
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-03 20:15:11 +0200

    better interface creation

--- imagesecrets/interface.py
+++ imagesecrets/interface.py
@@ -1,11 +1,9 @@
 """Application Programming Interface."""
 from __future__ import annotations
 
+import imagesecrets
 from fastapi import Depends, FastAPI, status
-
-import image_secrets
-from image_secrets.api import (
-    config,
+from imagesecrets.api import (
     dependencies,
     handlers,
     openapi,
@@ -13,56 +11,63 @@ from image_secrets.api import (
     schemas,
     tasks,
 )
-from image_secrets.api.routers import decode, encode, user
-from image_secrets.backend.database import base
-
-config_ = dependencies.get_config()
-app = FastAPI(
-    dependencies=[Depends(dependencies.get_config)],
-    title="ImageSecrets",
-    description="Encode and decode messages from images!",
-    version=image_secrets.__version__,
-    # will be set manually
-    docs_url=None,
-    redoc_url=None,
-    responses=responses.VALIDATION,  # type: ignore
-)
-
-# tortoise setup
-base.init(app, config_.pg_dsn)
-
-app.include_router(decode.router)
-app.include_router(encode.router)
-app.include_router(user.main)
-app.include_router(user.me)
-
-handlers.init(app)
-tasks.init(app)
+from imagesecrets.api.routers import decode, encode, user
+from imagesecrets.config import Settings, settings
+from imagesecrets.database import base
 
 
-@app.get(
-    "/",
-    response_model=schemas.Info,
-    status_code=status.HTTP_200_OK,
-    summary="Basic information about the API.",
-    tags=["home"],
-)
-async def home(
-    settings: config.Settings = Depends(dependencies.get_config),
-) -> dict[str, str]:
-    """Return basic info about the API."""
-    return {
-        "AppName": settings.app_name,
-        "SwaggerUI": settings.swagger_url,
-        "ReDoc": settings.redoc_url,
-        "GitHub": settings.repository_url,
-    }
+def create_api(config: Settings) -> FastAPI:
+    api = FastAPI(
+        dependencies=[Depends(dependencies.get_config)],
+        title="ImageSecrets",
+        description="Encode and decode messages from images!",
+        version=imagesecrets.__version__,
+        # will be set manually
+        docs_url=None,
+        redoc_url=None,
+        responses=responses.VALIDATION,  # type: ignore
+    )
+
+    # tortoise setup
+    base.init(api)
+
+    api.include_router(decode.router)
+    api.include_router(encode.router)
+    api.include_router(user.main)
+    api.include_router(user.me)
+
+    handlers.init(api)
+    tasks.init(api)
+
+    @api.get(
+        "/",
+        response_model=schemas.Info,
+        status_code=status.HTTP_200_OK,
+        summary="Basic information about the API.",
+        tags=["home"],
+    )
+    async def home(
+        settings: Settings = Depends(dependencies.get_config),
+    ) -> dict[str, str]:
+        """Return basic info about the API."""
+        return {
+            "AppName": settings.app_name,
+            "SwaggerUI": settings.swagger_url,
+            "ReDoc": settings.redoc_url,
+            "GitHub": settings.repository_url,
+        }
+
+    api.openapi = openapi.custom(api, swagger=True, redoc=True)  # type: ignore
+
+    return api
 
 
-app.openapi = openapi.custom(app, swagger=True, redoc=True)  # type: ignore
+def create_application():
+    """Create the application."""
+    return create_api(settings)
 
 
-__all__ = [
-    "app",
-    "home",
-]
+app = create_application()
+
+
+__all__ = ["app", "create_application", "create_api"]
