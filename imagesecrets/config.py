8d981154f1951b0a2028071af0f86e45c66941d3
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-06 22:08:13 +0200

    configuration

---
+++ imagesecrets/config.py
@@ -0,0 +1,66 @@
+"""FastAPI configuration."""
+from __future__ import annotations
+
+import os
+from typing import cast
+
+from fastapi_mail import ConnectionConfig, config
+from imagesecrets.constants import MESSAGE_DELIMITER, TEMPLATES
+from pydantic import BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+
+# monkey patch one of fastapi_mail path validation functions
+# because it is broken when application is not run from parent folder
+config.validate_path = lambda _: True
+
+
+def asyncpg_engine_dsn(db_url: str) -> str:
+    """Return Database URL with asyncpg engine.
+
+    :param db_url: Base database URL
+
+    """
+    split = db_url.split(":")
+    split[0] = f"{split[0]}ql+asyncpg"
+    return ":".join(split)
+
+
+class Settings(BaseSettings):
+    """API Settings."""
+
+    app_name: str = "ImageSecrets"
+
+    message_delimiter: str = MESSAGE_DELIMITER
+
+    pg_dsn: PostgresDsn = cast(PostgresDsn, os.getenv("DATABASE_URL"))
+    secret_key: str = cast(str, os.getenv("SECRET_KEY"))
+
+    icon_url: HttpUrl = cast(HttpUrl, os.getenv("ICON_URL"))
+    swagger_url: HttpUrl = cast(HttpUrl, os.getenv("SWAGGER_URL"))
+    redoc_url: HttpUrl = cast(HttpUrl, os.getenv("REDOC_URL"))
+    repository_url: HttpUrl = cast(HttpUrl, os.getenv("REPOSITORY_URL"))
+
+    @validator("pg_dsn")
+    def postgres_engine(cls, v: str) -> str:
+        return asyncpg_engine_dsn(db_url=v)
+
+    @staticmethod
+    def email_config() -> ConnectionConfig:
+        """Return email connection configuration."""
+        return ConnectionConfig(
+            MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+            MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+            MAIL_PORT=int(cast(int, os.getenv("MAIL_PORT"))),
+            MAIL_SERVER=os.getenv("MAIL_SERVER"),
+            MAIL_TLS=True,
+            MAIL_SSL=False,
+            MAIL_FROM=EmailStr(os.getenv("MAIL_FROM")),
+            TEMPLATE_FOLDER=str(TEMPLATES),
+        )
+
+
+settings = Settings()
+
+
+__all__ = [
+    "Settings",
+]
