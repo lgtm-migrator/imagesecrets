59323092a2d4de1933bd5638cf63acb4ecc29daf
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-12 11:43:06 +0200

    test `get` decoded_images

--- tests/test_api/test_routes/conftest.py
+++ tests/test_api/test_routes/conftest.py
@@ -35,3 +35,37 @@ def return_user() -> User:
         created=datetime(year=2000, month=1, day=1),
         updated=datetime(year=3000, month=2, day=2),
     )
+
+
+@pytest.fixture()
+def return_decoded(return_user) -> User:
+    """Return a fake database decodedimage entry."""
+    from imagesecrets.database.image.models import DecodedImage
+
+    return DecodedImage(
+        image_name="test_image_name",
+        message="test_message",
+        delimiter="test_delimiter",
+        lsb_amount=1,
+        filename="test_filename",
+        created=datetime(year=2000, month=1, day=1),
+        updated=datetime(year=3000, month=2, day=2),
+        user_id=return_user.id,
+    )
+
+
+@pytest.fixture()
+def return_encoded(return_user) -> User:
+    """Return a fake database encodedimage entry."""
+    from imagesecrets.database.image.models import EncodedImage
+
+    return EncodedImage(
+        image_name="test_image_name",
+        message="test_message",
+        delimiter="test_delimiter",
+        lsb_amount=1,
+        filename="test_filename",
+        created=datetime(year=2000, month=1, day=1),
+        updated=datetime(year=3000, month=2, day=2),
+        user_id=return_user.id,
+    )
